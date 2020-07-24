6a6a521fca04aab25f59655bfc41f4b44bd8edeb
maw1a <amawia15@gmail.com>
2020-07-24 06:07:28 +0530

    navbar --done

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnDJPZYkvuenBxs-wpVZTwweqDLdOLZJE",
+    authDomain: "alpha-a2.firebaseapp.com",
+    databaseURL: "https://alpha-a2.firebaseio.com",
+    projectId: "alpha-a2",
+    storageBucket: "alpha-a2.appspot.com",
+    messagingSenderId: "173039203818",
+    appId: "1:173039203818:web:95aaad4c213c6e8a4a2e93",
+    measurementId: "G-ELN65T0TPS"
+};
+
+firebase.initializeApp(firebaseConfig);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import {
+  AppBar,
+  Toolbar,
+  InputBase,
+  Button,
+  Typography
+} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
 
 export default function Navbar() {
     return(
-        <nav className="Nav-container">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/allchats">All Chats</Link>
-            </li>
-          </ul>
-        </nav>
+      <AppBar position="static">
+        <Toolbar style={{
+          height: 60,
+          backgroundColor: 'rgb(255, 0, 0)',
+          justifyContent: 'space-between'
+        }}>
+          <a href="/" style={{
+            fontSize: 24,
+            textDecorationLine: 'none',
+            color: 'white'
+          }}>αlpha</a>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            backgroundColor: 'rgba(255,255,255, 0.4)',
+            paddingTop: 8,
+            paddingBottom: 5,
+            paddingLeft: 15,
+            paddingRight: 15,
+            width: '30vw',
+            borderRadius: 4
+          }}>
+            <div style={{marginTop: 3, marginRight: 10}}>
+              <SearchIcon style={{fontSize: 26}}/>
+            </div>
+            <InputBase
+              placeholder="Search…"
+              style={{ fontSize: 18, color: 'white', flex: 1 }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div style={{margin: 0, alignItems: 'center', display: 'flex', flexDirection: 'row'}}>
+            <Button disableElevation variant="contained" href="/login" style={{color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.4)', textTransform: 'none'}}>Login</Button>
+            <Button variant="outlined" href="/signup" style={{marginLeft: 10, textTransform: 'none', color: 'white', borderColor: 'white'}}>Sign up</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
     );
 }
